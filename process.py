09d4f74086bc1cc5ab916e2df2de3bb661b4887a
KUMA <tienquanphan2003@gmail.com>
2022-10-25 19:15:32 +0700

    change file

--- process.py
+++ process.py
@@ -2,9 +2,7 @@ import cv2
 import numpy as np
 import Preprocess
 import math
-#quan la toi
 # import char_license_plate_recognition
-
 npaClassifications = np.loadtxt("classifications.txt", np.float32)
 npaFlattenedImages = np.loadtxt("flattened_images.txt", np.float32) 
 npaClassifications = npaClassifications.reshape((npaClassifications.size, 1))# reshape numpy array to 1d, necessary to pass to call to train
