8a56ad6e88536726c59698deb1fe3f5f0ad6a10c
KUMA <tienquanphan2003@gmail.com>
2022-10-27 02:49:37 +0700

    update

--- Gen.py
+++ Gen.py
@@ -23,8 +23,8 @@ Max_ratio_char = 0.7
 RESIZE_IMG_WIDTH = 20
 RESIZE_IMG_HEIGHT = 30
 
-npaClassifications = np.loadtxt("classifications.txt", np.float32)
-npaFlattenedImages = np.loadtxt("flattened_images.txt", np.float32) 
+npaClassifications = np.loadtxt("./data/classifications.txt", np.float32)
+npaFlattenedImages = np.loadtxt("./data/flattened_images.txt", np.float32) 
 npaClassifications = npaClassifications.reshape((npaClassifications.size, 1))# reshape numpy array to 1d, necessary to pass to call to train
 kNearest = cv2.ml.KNearest_create()                   # instantiate KNN object
 kNearest.train(npaFlattenedImages, cv2.ml.ROW_SAMPLE, npaClassifications)
